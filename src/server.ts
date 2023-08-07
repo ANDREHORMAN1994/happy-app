9857dad140e65d5747630065cf84c5d92aac86b8
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-08-07 17:58:56 -0300

    :recycle: refactor: atualizando server

--- src/server.ts
+++ src/server.ts
@@ -29,15 +29,28 @@ server.listen(port, () => {
   console.log('Entre aqui:', 'http://localhost:5500/');
 });
 
-// /* ATENÇÃO : 
-// O nodemon pode crashar, Erro: Error: listen EADDRINUSE :::5500
-// Significa que a porta já está em uso. Para resolver isso você pode 
-// encerrar o processo que está rodando nessa porta executando o comando :
-//   pkill node
+/* ATENÇÃO : 
+O nodemon pode crashar: Erro: Error: listen EADDRINUSE :::5500
 
-// Caso o Cache esteja cheio usar :
-//   npm cache clean --force
+Significa que a porta já está em uso. Para resolver isso você pode 
+encerrar o processo que está rodando nessa porta executando o comando:
+  -> pkill node
 
-// Depois pode apagar o node_modules e o package-lock.json e rodar :
-//   npm install
-// */
+Caso o Cache esteja cheio usar:
+  -> npm cache clean --force
+
+Depois pode apagar o node_modules e o package-lock.json e rodar:
+  -> npm install
+
+Execute o container:
+  -> docker pull postgres
+  -> docker run --name postgre-db -e POSTGRES_PASSWORD=happy -p 5432:5432 -d postgres
+  -> docker ps -a
+
+Execute o prisma:
+  -> npx prisma migrate dev
+  -> npx prisma studio
+
+Rode a aplicação:
+  -> npm run dev
+*/
